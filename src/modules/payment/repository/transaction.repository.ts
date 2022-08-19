b57ddadee93eb7612ddb8b504789970eb66d693d
Rogerio Coelho <rogerio@tidea.com.br>
2022-08-19 10:46:15 -0300

    Implementacoes

---
+++ src/modules/payment/repository/transaction.repository.ts
@@ -0,0 +1,28 @@
+
+import Transaction from "../domain/transaction";
+import transaction from "../domain/transaction";
+import PaymentGateway from "../gateway/payment.gateway";
+import TransactionModel from "./transaction.model";
+
+export default class TransactionRepository implements PaymentGateway {
+    async save(input: transaction): Promise<transaction> {
+        await TransactionModel.create({
+            id: input.id.id,
+            orderId: input.orderId,
+            amount: input.amount,
+            status: input.status,
+            createdAt: input.createdAt,
+            updatedAt: input.updatedAt
+        });
+
+        return new Transaction({
+            id: input.id,
+            orderId: input.orderId,
+            amount: input.amount,
+            status: input.status,
+            createdAt: input.createdAt,
+            updatedAt: input.updatedAt
+        });
+    }
+    
+}
